c66b364f6719983a14e7af7e02c2204c937fecf0
Florian MARTINEZ <florianmartinez@MacBook-Pro-de-Florian.local>
2023-04-12 17:21:43 +0200

    udapte1

--- asset/js/modules/game.js
+++ asset/js/modules/game.js
@@ -1,19 +1,21 @@
-import { Background } from './background.js'
+import { Background } from "./background.js";
+import { Player } from "./player.js";
 
 export class Game {
-    score = 0
-    gameOver = false
-    lastTimeStamp = 0
+    score = 0;
+    gameOver = false;
+    lastTimeStamp = 0;
 
     constructor() {
         /** @type {HTMLCanvasElement} */
-        this.canvas = document.querySelector('canvas')
-        this.canvas.width = 480
-        this.canvas.height = 360
+        this.canvas = document.querySelector("canvas");
+        this.canvas.width = 480;
+        this.canvas.height = 360;
 
-        this.ctx = this.canvas.getContext('2d')
-        this.background = new Background(this.ctx)
-        this.animate(0)
+        this.ctx = this.canvas.getContext("2d");
+        this.background = new Background(this.ctx);
+        this.player = new Player(this.ctx);
+        this.animate(0);
     }
 
     /**
@@ -21,11 +23,14 @@ export class Game {
      * Nombre de millisecondes écoulées depuis le début du jeu
      */
     animate = (timeStamp) => {
-        const deltaTime = timeStamp - this.lastTimeStamp
-        this.lastTimeStamp = timeStamp
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-        this.background.draw()
-        this.background.update(deltaTime)
-        window.requestAnimationFrame(this.animate)
-    }
+        const deltaTime = timeStamp - this.lastTimeStamp;
+        this.lastTimeStamp = timeStamp;
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.background.draw();
+        this.background.update(deltaTime);
+        this.player.draw();
+        this.player.update(timeStamp);
+
+        window.requestAnimationFrame(this.animate);
+    };
 }
--- asset/js/modules/player.js
+++ asset/js/modules/player.js
@@ -1,6 +1,44 @@
 export class Player {
-    constructor() {
-        this.image = new Image()
-        this.image.src = './assets/img/player-spritesheet.png'
+    sourceX = 0;
+    sourceY = 0;
+    frameWidth = 345;
+    frameHeight = 150;
+    destinationX = 0;
+    destinationY = 100;
+
+    fps = 1000 / 12;
+    framesLength = 7;
+    FrameIndex = 0;
+    /**
+     *
+     * @param {CanvasRenderingContext2D} ctx
+     */
+
+    constructor(ctx) {
+        this.image = new Image();
+        this.image.src = "./asset/img/player.png";
+        this.ctx = ctx;
+    }
+    draw() {
+        this.ctx.drawImage(
+            this.image,
+            this.sourceX,
+            this.sourceY,
+            this.frameWidth,
+            this.frameHeight,
+            this.destinationX,
+            this.destinationY,
+            this.frameWidth,
+            this.frameHeight
+        );
+    }
+
+    /**
+     *
+     * @param {number} timeStamp
+     */
+    update(timeStamp) {
+        const frameIndex = Math.floor(timeStamp / this.fps) % this.framesLength;
+        this.sourceX = frameIndex * this.frameWidth;
     }
 }
